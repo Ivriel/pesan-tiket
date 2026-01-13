9224d5e82e6a9bf1c524474201243bd8ae1f03f0
Ivriel <gunawanivriel@gmail.com>
2026-01-13 19:19:43 +0700

    Buat greeting di dashboard dan ubah route ke beranda jadi ke daftar booking abis dari detail booking

--- resources/views/bookings/show.blade.php
+++ resources/views/bookings/show.blade.php
@@ -119,8 +119,8 @@
                             </svg>
                             Cetak Tiket
                         </button>
-                        <a href="{{ route('dashboard') }}" class="flex-1 bg-gray-100 dark:bg-gray-700 text-gray-600 dark:text-gray-200 font-bold py-3.5 px-6 rounded-xl hover:bg-gray-200 dark:hover:bg-gray-600 transition text-center flex items-center justify-center">
-                            Kembali ke Beranda
+                        <a href="{{ route('bookings.listd') }}" class="flex-1 bg-gray-100 dark:bg-gray-700 text-gray-600 dark:text-gray-200 font-bold py-3.5 px-6 rounded-xl hover:bg-gray-200 dark:hover:bg-gray-600 transition text-center flex items-center justify-center">
+                            Kembali ke Daftar Booking
                         </a>
                     </div>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BookingController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RouteController;
 use App\Http\Controllers\ScheduleController;
@@ -13,9 +14,7 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
     Route::resource('types', TypeController::class)->except('show');
     Route::resource('routes', RouteController::class)->except('show');
